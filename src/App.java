123c510d5e7baf96082a520b239785bff8678aa5
Tryhards <101475765+TryhardsAccount@users.noreply.github.com>
2022-03-26 17:42:36 -0400

    Updates

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ import NetWork.DataBaseMovie;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         DataBaseMovie.updateInfoMovie("2DMovie1", "FastandFurios", "NameMovie");
+        DataBaseMovie.updateInfoMovie("2DMovie2", "SpiderMan", "NameMovie");
     }
 }
